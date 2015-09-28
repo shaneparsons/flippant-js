0b8f6ad41b9e1a52a7a3b4b32f7a1ff364c603ce
Shane Parsons <s.parsons@hotmail.ca>
2015-09-28 10:41:42 -0400

    Fixes broken funtionality
    Shit wasn't working properly, this fixed it for me.
    These changes were make a long time ago so I can't exactly remember what they were,

--- flippant.js
+++ flippant.js
@@ -1,83 +1,152 @@
-(function(e){if("function"==typeof bootstrap)bootstrap("flippant",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeFlippant=e}else"undefined"!=typeof window?window.flippant=e():global.flippant=e()})(function(){var define,ses,bootstrap,module,exports;
-return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
-exports.flip = flip
+(function (e) {
+    if ("function" == typeof bootstrap) bootstrap("flippant", e);
+    else if ("object" == typeof exports) module.exports = e();
+    else if ("function" == typeof define && define.amd) define(e);
+    else if ("undefined" != typeof ses) {
+        if (!ses.ok()) return;
+        ses.makeFlippant = e
+    } else "undefined" != typeof window ? window.flippant = e() : global.flippant = e()
+})(function () {
+    var define, ses, bootstrap, module, exports;
+    return (function (e, t, n) {
+        function i(n, s) {
+            if (!t[n]) {
+                if (!e[n]) {
+                    var o = typeof require == "function" && require;
+                    if (!s && o) return o(n, !0);
+                    if (r) return r(n, !0);
+                    throw new Error("Cannot find module '" + n + "'")
+                }
+                var u = t[n] = {
+                    exports: {}
+                };
+                e[n][0].call(u.exports, function (t) {
+                    var r = e[n][1][t];
+                    return i(r ? r : t)
+                }, u, u.exports)
+            }
+            return t[n].exports
+        }
 
-function flip(flipper, content, type, class_name, timeout) {
-  var position
-    , back
-    , style_func
-  timeout = timeout || 400
-  type = type || "card"
+        var r = typeof require == "function" && require;
+        for (var s = 0; s < n.length; s++) i(n[s]);
+        return i
+    })({
+        1: [function (require, module, exports) {
 
-  if (type === "modal") {
-    class_name = class_name || "flippant-modal-dark"
-    position = "fixed"
-    style_func = null_styles
-  }
+            exports.flip = flip
 
-  if (type === "card") {
-    class_name = class_name || "flippant-modal-light"
-    position = "absolute"
-    style_func = card_styles
-  }
+            //-------------------
 
-  back = document.createElement('div')
-  document.body.appendChild(back)
-  set_styles(back, flipper, position)
-  back.innerHTML = content
+            function flip(flipper, content, type, class_name, timeout) { // flipper, content, type, container
+                var position, back, style_func
+                timeout = timeout || 400
+                type = type || "card"
+                // container = container || null // here
 
-  flipper.classList.add('flippant')
-  back.classList.add('flippant-back')
-  back.classList.add(class_name)
-  if (position == "absolute") {
-    style_func(back)
-  } else {
-    window.setTimeout(function () {
-      style_func(back)
-    }, 0)
-  }
-  window.setTimeout(function () {
-    back.classList.add('flipper')
-    back.classList.add('flipped')
-    flipper.classList.add('flipped')
-  }, 0)
+                if (type === "modal") {
+                    class_name = class_name || "flippant-modal-dark"
+                    position = "fixed"
+                    style_func = null_styles
+                }
 
-  back.addEventListener('close', close)
-  back.close = close
+                if (type === "card") {
+                    class_name = class_name || "flippant-modal-light"
+                    position = "absolute"
+                    style_func = card_styles
+                }
 
-  function close() {
-    set_styles(back, flipper, position)
-    back.classList.remove('flipped')
-    back.classList.remove('flipped')
-    flipper.classList.remove('flipped')
-    window.setTimeout(function () {
-      back.classList.remove(class_name)
-      document.body.removeChild(back)
-    }, timeout)
-  }
+                back = document.createElement('div')
 
-  return back
-}
+                /*
+                 container = document.getElementById(container); // here
 
-function set_styles(back, front, position) {
-  back.style.position = position
-  back.style.top = front.offsetTop + "px"
-  back.style.left = front.offsetLeft + "px"
-  back.style['min-height'] = front.offsetHeight + "px"
-  back.style.width = front.offsetWidth + "px"
-  back.style["z-index"] = 9999
-}
+                 if (container != null) {
+                 container.appendChild(back)
+                 } else {
+                 document.body.appendChild(back)
+                 }
+                 */
 
-function null_styles(back) {
-  back.style.top = null
-  back.style.left = null
-  back.style.height = null
-  back.style.width = null
-}
+                document.body.appendChild(back)
 
-function card_styles(back) {
-  back.style.height = 'auto'
-}
-},{}]},{},[1])(1)
+                set_styles(back, flipper, position)
+                back.innerHTML = content
+                flipper.classList.add('flippant')
+                back.classList.add('flippant-back')
+                back.classList.add(class_name)
+
+                if (position == "absolute") {
+                    style_func(back)
+
+                } else {
+                    window.setTimeout(function () {
+                        style_func(back)
+                    }, 0)
+                }
+                window.setTimeout(function () {
+                    back.classList.add('flipper')
+                    back.classList.add('flipped')
+                    flipper.classList.add('flipped')
+                }, 0)
+
+                back.addEventListener('close', close)
+                back.close = close
+
+                function close() {
+                    set_styles(back, flipper, position)
+                    back.classList.remove('flipped')
+                    back.classList.remove('flipped')
+                    flipper.classList.remove('flipped')
+                    window.setTimeout(function () {
+                        back.classList.remove(class_name)
+
+                        /*
+                         if (container != null) { // here
+                         container.removeChild(back)
+                         } else {
+                         document.body.removeChild(back)
+                         }
+                         */
+
+                        document.body.removeChild(back)
+
+                    }, timeout)
+                }
+
+                return back
+            }
+
+            function set_styles(back, front, position) {
+                back.style.position = position
+
+                cur = front
+                totalLeft = 0
+                totalTop = 0
+                while (cur) {
+                    totalLeft += cur.offsetLeft;
+                    totalTop += cur.offsetTop;
+                    cur = cur.offsetParent;
+                }
+                back.style.top = totalTop + "px"
+                back.style.left = totalLeft + "px"
+
+                back.style['min-height'] = front.offsetHeight + "px"
+                back.style.width = front.offsetWidth + "px"
+                back.style["z-index"] = 100
+            }
+
+            function null_styles(back) {
+                back.style.top = null
+                back.style.left = null
+                back.style.height = null
+                back.style.width = null
+            }
+
+            function card_styles(back) {
+                back.style.height = 'auto'
+            }
+        }, {}]
+    }, {}, [1])(1)
 });
 ;
